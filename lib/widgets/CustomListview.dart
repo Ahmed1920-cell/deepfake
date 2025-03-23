638c157a7944e157e190d14ec5597c5aa1ad6db2
ahmed <ahmeddarwesh317@yahoo.com>
2025-03-23 14:06:24 +0200

    Initial commit

---
+++ lib/widgets/CustomListview.dart
@@ -0,0 +1,19 @@
+import 'package:deepfake/model/DeepModel.dart';
+import 'package:deepfake/widgets/CustomCard.dart';
+import 'package:deepfake/widgets/text_widget.dart';
+import 'package:flutter/material.dart';
+
+import '../constant/color.dart';
+
+class CustomListview extends StatelessWidget {
+   CustomListview({super.key,required this.data});
+List<DeepModel> data;
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      //reverse: true,
+      itemCount: data.length,
+      itemBuilder: (context,index)=>Customcard(info: data[index], index: index,),);
+  }
+}
